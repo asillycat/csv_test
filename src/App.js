0661e98d1d806b73b1492c4e9da47b911b4b355c
asillycat <84633626+asillycat@users.noreply.github.com>
2021-07-15 18:14:54 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import SampleChart from './chart';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SampleChart></SampleChart>
+      </header>
+    </div>
+  );
+}
+
+export default App;
